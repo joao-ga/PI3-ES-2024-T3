d35c45f60a2701f16f68071406142e739d11d6ff
Enzo Quatrochi <enzoquatrochi@outlook.com>
2024-03-17 21:58:13 -0300

    função de logout feita

--- app/src/main/java/br/com/the_guardian/homeScreen.kt
+++ app/src/main/java/br/com/the_guardian/homeScreen.kt
@@ -4,11 +4,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.widget.AppCompatButton
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class homeScreen : AppCompatActivity() {
 
     private lateinit var btnVoltar: AppCompatButton
     private lateinit var btnCadastrarCartao: AppCompatButton
+    private lateinit var btnSair: AppCompatButton
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_screen)
@@ -21,7 +26,12 @@ class homeScreen : AppCompatActivity() {
         btnCadastrarCartao.setOnClickListener {
             nextScreen(RegisterCreditCard::class.java)
         }
-
+        btnSair = findViewById(R.id.btnSair)
+        btnSair.setOnClickListener {
+            auth = Firebase.auth
+            auth.signOut()
+            nextScreen(loginScreen::class.java)
+        }
     }
 
     private fun nextScreen(screen: Class<*>) {
