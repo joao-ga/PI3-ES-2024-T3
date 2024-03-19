68a598f3cad28951070234de0a60939af5a6f679
joaogabiazon <jbiazonferreira@gmail.com>
2024-03-19 14:20:42 -0300

    Put the maps in the home screen

--- app/src/main/java/br/com/the_guardian/homeScreen.kt
+++ app/src/main/java/br/com/the_guardian/homeScreen.kt
@@ -5,7 +5,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.widget.AppCompatButton
 
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
 class homeScreen : AppCompatActivity() {
+
+    private lateinit var mMap: GoogleMap
 
     private lateinit var btnVoltar: AppCompatButton
     private lateinit var btnCadastrarCartao: AppCompatButton
@@ -22,6 +31,17 @@ class homeScreen : AppCompatActivity() {
             nextScreen(RegisterCreditCard::class.java)
         }
 
+    }
+
+    fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions()
+            .position(sydney)
+            .title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 
     private fun nextScreen(screen: Class<*>) {
