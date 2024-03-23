ba63dff12c2f610aa71fedb83564d954f832390e
joaogabiazon <jbiazonferreira@gmail.com>
2024-03-22 23:39:31 -0300

    Set user location as default

--- app/src/main/java/br/com/the_guardian/homeScreen.kt
+++ app/src/main/java/br/com/the_guardian/homeScreen.kt
@@ -1,11 +1,18 @@
 package br.com.the_guardian
 
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
-import android.util.Log
+import android.provider.Settings
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -17,7 +24,6 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
 
 class homeScreen : AppCompatActivity(), OnMapReadyCallback {
-
     private val places = arrayListOf(
         Place("Google", LatLng(-23.5868031,-46.6847268), "Av. Brig. Faria Lima, 3477 - 18º Andar - Itaim Bibi, São Paulo - SP, 04538-133", 4.8f),
         Place("Parque", LatLng(-23.5902467, -46.6639756), "Av. República do Líbano, 1157 - Ibirapuera, São Paulo - SP, 04502-001", 4.9f)
@@ -31,6 +37,8 @@ class homeScreen : AppCompatActivity(), OnMapReadyCallback {
     )
 
     private lateinit var mMap: GoogleMap
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var userLoc: LatLng
 
     private lateinit var btnCadastrarCartao: AppCompatButton
     private lateinit var btnSair: AppCompatButton
@@ -38,6 +46,10 @@ class homeScreen : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_screen)
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
+        getCurrentLocation()
 
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
@@ -67,6 +79,15 @@ class homeScreen : AppCompatActivity(), OnMapReadyCallback {
                     .position(place.latLng)
             )
         }
+
+        // Adiciona marcador para a localização do usuário
+        if (::userLoc.isInitialized) {
+            googleMap.addMarker(
+                MarkerOptions()
+                    .title("Sua Localização")
+                    .position(userLoc)
+            )
+        }
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
@@ -80,8 +101,12 @@ class homeScreen : AppCompatActivity(), OnMapReadyCallback {
                         .position(place.latLng)
                 )
             }
-            val firstPlace = places[0]
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(firstPlace.latLng, 12f))
+            if(::userLoc.isInitialized) {
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLoc, 12f))
+
+                mMap.addMarker(MarkerOptions().position(userLoc).title("Sua Localização"))
+
+            }
         }
     }
 
@@ -90,12 +115,101 @@ class homeScreen : AppCompatActivity(), OnMapReadyCallback {
         startActivity(newScreem)
 
     }
-
     private fun usuarioEstaLogado(): Boolean {
-        Log.i("Entrei","Entrei na funcao")
         val auth = FirebaseAuth.getInstance()
         val usuarioAtual = auth.currentUser
         return usuarioAtual != null
+    }
+
+    // funcoes de pegar a localizacao do usuario
+
+    private fun getCurrentLocation() {
+        if(checkPermission()) {
+            if(isLocationEnabled()) {
+                fusedLocationProviderClient.lastLocation.addOnCompleteListener(this) {task->
+                    val location: Location?= task.result
+                    if(location == null) {
+                        Toast.makeText(this, "Null Recived", Toast.LENGTH_SHORT).show()
+
+                    } else {
+                        Toast.makeText(this, "Get Success", Toast.LENGTH_SHORT).show()
+                        userLoc = LatLng(location.latitude, location.longitude)
+                        // move a camera para a localizaçao do usuario
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLoc, 12f))
+                        // Adiciona marcador do usuário
+                        mMap.addMarker(MarkerOptions().position(userLoc).title("Sua Localização"))
+
+                    }
+
+                }
+
+
+            } else {
+                //setting open here
+                Toast.makeText(this, "Turn on location", Toast.LENGTH_SHORT).show()
+                val intent=Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
+            }
+
+        } else {
+            //request permission
+
+            requestPermissions()
+        }
+
+    }
+
+    companion object {
+        private var PERMISSION_REQUEST_ACCESS_LOCATION = 100
+    }
+
+    private fun isLocationEnabled():Boolean {
+        val locationManager:LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+
+    }
+    private fun requestPermissions() {
+
+        ActivityCompat.requestPermissions(
+            this, arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                android.Manifest.permission.ACCESS_FINE_LOCATION),
+            PERMISSION_REQUEST_ACCESS_LOCATION
+        )
+
+
+    }
+
+    private fun checkPermission(): Boolean {
+        if(ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+            return true
+        }
+
+        return false
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if(requestCode== PERMISSION_REQUEST_ACCESS_LOCATION) {
+            if(grantResults.isNotEmpty() && grantResults[0]==PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(applicationContext, "Granted", Toast.LENGTH_SHORT).show()
+                getCurrentLocation()
+            } else {
+                Toast.makeText(applicationContext, "Denied", Toast.LENGTH_SHORT).show()
+
+            }
+        }
+
     }
 
 }
