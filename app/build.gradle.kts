4f50275cee26dfec79003849af25396f90efcaef
joaogabiazon <jbiazonferreira@gmail.com>
2024-03-29 13:44:28 -0300

    Keep user logged

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,4 +59,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
 }
