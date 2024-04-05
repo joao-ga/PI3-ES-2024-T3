f0dcb7fc85ec53a365d3c02a46d01b8eb8d5ed77
Enzo Quatrochi <enzoquatrochi@outlook.com>
2024-04-04 23:06:30 -0300

    geração do qrcode

---
+++ app/src/main/java/br/com/the_guardian/QrCodeScreen.kt
@@ -0,0 +1,61 @@
+package br.com.the_guardian
+
+import android.graphics.Bitmap
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.RadioButton
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.MultiFormatWriter
+import com.google.zxing.WriterException
+import com.google.zxing.common.BitMatrix
+
+class QrCodeScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_qr_code_screen)
+
+        val checkedRadioButtonText = intent.getStringExtra("checkedRadioButtonText")
+        if (checkedRadioButtonText != null) {
+
+            val qrCode = findViewById<ImageView>(R.id.qrCode)
+            val codigo = 4532  // adaptar para cada qrCode gerado
+            val text = codigo
+            try {
+                val bitmap = textToImageEncode(text)
+                qrCode.setImageBitmap(bitmap)
+            } catch (e: WriterException) {
+                e.printStackTrace()
+            }
+        }
+
+    }
+
+    @Throws(WriterException::class)
+    private fun textToImageEncode(value: Int): Bitmap? {
+        val bitMatrix: BitMatrix
+        try {
+            bitMatrix = MultiFormatWriter().encode(
+                value.toString(),
+                BarcodeFormat.QR_CODE,
+                500, 500, null
+            )
+        } catch (illegalArgumentException: IllegalArgumentException) {
+            return null
+        }
+        val bitMatrixWidth = bitMatrix.width
+        val bitMatrixHeight = bitMatrix.height
+        val pixels = IntArray(bitMatrixWidth * bitMatrixHeight)
+        for (y in 0 until bitMatrixHeight) {
+            val offset = y * bitMatrixWidth
+            for (x in 0 until bitMatrixWidth) {
+                pixels[offset + x] = if (bitMatrix[x, y]) -0x1000000 else -0x1
+            }
+        }
+        val bitmap = Bitmap.createBitmap(bitMatrixWidth, bitMatrixHeight, Bitmap.Config.ARGB_4444)
+        bitmap.setPixels(pixels, 0, 500, 0, 0, bitMatrixWidth, bitMatrixHeight)
+        return bitmap
+
+    }
+
+}
--- app/src/main/java/br/com/the_guardian/markerInfoAdapter.kt
+++ app/src/main/java/br/com/the_guardian/markerInfoAdapter.kt
@@ -1,6 +1,7 @@
 package br.com.the_guardian
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.Button
@@ -8,22 +9,31 @@ import android.widget.TextView
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.Marker
 
-class markerInfoAdapter(private val context: Context) : GoogleMap.InfoWindowAdapter{
+class markerInfoAdapter(private val context: Context) : GoogleMap.InfoWindowAdapter {
     override fun getInfoContents(marker: Marker): View? = null
 
     override fun getInfoWindow(marker: Marker): View? {
-
         val place = marker.tag as? homeScreen.Place ?: return null
-
         val view = LayoutInflater.from(context).inflate(R.layout.activity_marker_info, null)
 
         view.findViewById<TextView>(R.id.marker_title).text = place.name
         view.findViewById<TextView>(R.id.marker_reference).text = place.reference
         view.findViewById<TextView>(R.id.marker_adress).text = place.address
-        view.findViewById<TextView>(R.id.marker_disponibility).text = "Está disponível: ${place.disponibility}"
-        view.findViewById<Button>(R.id.btn_consultar)
+        view.findViewById<TextView>(R.id.marker_disponibility).text = if (place.disponibility) "Está disponível: Sim"
+        else "Está disponível: Não"
+
+        val btnConsultar = view.findViewById<Button>(R.id.btn_consultar)
+        btnConsultar.setOnClickListener {
+            val intent = Intent(context, DataScreen::class.java).apply {
+                putExtra("name", place.name)
+                putExtra("reference", place.reference)
+                putExtra("address", place.address)
+                putExtra("disponibility", place.disponibility)
+            }
+            context.startActivity(intent)
+        }
+
         return view
     }
-
 
 }
