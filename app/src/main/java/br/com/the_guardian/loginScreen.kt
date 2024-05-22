ef247a44eb27efa98c39959365306940d8a0b365
annacarolinmae <carolinamorellichaves@gmail.com>
2024-05-22 20:21:27 -0300

    Validação e padronização de erros da tela de login

--- app/src/main/java/br/com/the_guardian/loginScreen.kt
+++ app/src/main/java/br/com/the_guardian/loginScreen.kt
@@ -5,7 +5,10 @@ import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.graphics.Color
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.util.Patterns
 import android.view.View
@@ -14,9 +17,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatEditText
 import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.content.ContextCompat
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseAuthException
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
@@ -37,6 +42,9 @@ class loginScreen : AppCompatActivity() {
     private lateinit var tvEsqueceuSenha: AppCompatTextView
     private lateinit var tvStatusEsqueceuSenha: AppCompatTextView
     private lateinit var tvEntrarAnonimamente: AppCompatTextView
+    private lateinit var tvEmailLoginError: AppCompatTextView
+    private lateinit var tvESenhaLoginError: AppCompatTextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -60,21 +68,41 @@ class loginScreen : AppCompatActivity() {
         tvEsqueceuSenha = findViewById(R.id.tvEsqueceuSenha)
         tvStatusEsqueceuSenha = findViewById(R.id.tvStatusEsqueceuSenha)
 
+        tvEmailLoginError = findViewById(R.id.tvEmailLoginError)
+        tvESenhaLoginError = findViewById(R.id.tvESenhaLoginError)
+
+        clearErrorOnTextChange(etEmailLogin, tvEmailLoginError)
+        clearErrorOnTextChange(etSenhaLogin, tvESenhaLoginError)
+
         DataScreen.locacaoConfirmada = false
         getLocationInfos()
 
         btnEnviarLogin.setOnClickListener { view ->
             val email = etEmailLogin.text.toString()
             val senha = etSenhaLogin.text.toString()
-            if (!isEmailValid(email) && !isPasswordValid(senha)) {
-                val snackbar = Snackbar.make(view, "Email ou senha inválidos!", Snackbar.LENGTH_SHORT)
+
+            var hasError = false
+
+            if (!isEmailValid(email)) {
+                updateInputState(etEmailLogin, tvEmailLoginError, "Email inválido", true)
+                hasError = true
+            } else {
+                updateInputState(etEmailLogin, tvEmailLoginError, "", false)
+            }
+
+            if (!isPasswordValid(senha)) {
+                updateInputState(etSenhaLogin, tvESenhaLoginError, "Senha inválida", true)
+                hasError = true
+            } else {
+                updateInputState(etSenhaLogin, tvESenhaLoginError, "", false)
+            }
+
+            if (hasError) {
+                // Se houver mensagens de erro, mostra um snackbar
+                val snackbar = Snackbar.make(view, "Preencha todos os campos corretamente!", Snackbar.LENGTH_LONG)
                 snackbar.setBackgroundTint(Color.RED)
                 snackbar.show()
-            }else if (!isEmailValid(email)) {
-                etEmailLogin.error = "Email inválido"
-            }else if (!isPasswordValid(senha)) {
-                etSenhaLogin.error = "Senha inválida"
-            }else {
+            } else {
                 authenticator(email, senha)
             }
         }
@@ -89,34 +117,68 @@ class loginScreen : AppCompatActivity() {
         }
     }
 
+
     private fun isEmailValid(email: String): Boolean {
         return email.isNotEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()
     }
 
     private fun isPasswordValid(password: String): Boolean {
-        return password.isNotEmpty() && password.length >= 6
+        return password.isNotEmpty()
     }
 
     private fun recoverPassword(email: String) {
-        if(email.isEmpty()){
-            tvStatusEsqueceuSenha.text = "Digite um e-mail na caixa de texto a cima"
-            tvStatusEsqueceuSenha.visibility = View.VISIBLE
+        if (email.isEmpty()) {
+            showSnackbar(etEmailLogin, "Preencha todos os campos!", Color.RED)
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            updateInputState(etEmailLogin, tvEmailLoginError, "Digite um e-mail válido", true)
         } else {
             auth.sendPasswordResetEmail(email).addOnCompleteListener { task ->
                 if (!task.isSuccessful) {
                     val e = task.exception
-                    if (e is FirebaseAuthException) {
-                        val code = e.errorCode
-                        val message = e.localizedMessage
-                        Log.e("RecoverPassword", "Erro: $code, Mensagem: $message")
+                    val errorMessage = if (e is FirebaseAuthException) {
+                        when (e.errorCode) {
+                            "ERROR_INVALID_EMAIL" -> "O endereço de e-mail está mal formatado."
+                            "ERROR_USER_NOT_FOUND" -> "Usuário não encontrado."
+                            else -> "Falha ao enviar o e-mail de recuperação. Tente novamente mais tarde."
+                        }
+                    } else {
+                        "Falha ao enviar o e-mail de recuperação. Tente novamente mais tarde."
                     }
+                    Log.e("RecoverPassword", "Erro: ${e?.localizedMessage}")
+                    Toast.makeText(baseContext, errorMessage, Toast.LENGTH_SHORT).show()
                 } else {
-                    Toast.makeText(baseContext, "E-mail enviado", Toast.LENGTH_SHORT).show()
+                    updateInputState(etEmailLogin, tvStatusEsqueceuSenha, "", false)
+                    Toast.makeText(baseContext, "E-mail enviado com sucesso", Toast.LENGTH_SHORT).show()
                     Log.i("RecoverPassword", "E-mail enviado com sucesso")
                 }
             }
         }
     }
+
+    private fun showSnackbar(view: View, message: String, color: Int) {
+        val snackbar = Snackbar.make(view, message, Snackbar.LENGTH_LONG)
+        val snackbarView = snackbar.view
+        snackbarView.setBackgroundColor(color)
+        snackbar.show()
+    }
+
+
+    private fun updateInputState(editText: AppCompatEditText, errorTextView: AppCompatTextView, errorMessage: String, isError: Boolean) {
+        val errorIcon: Drawable? = if (isError) {
+            ContextCompat.getDrawable(this, R.drawable.baseline_error_24)?.apply {
+                setBounds(0, 0, intrinsicWidth, intrinsicHeight)
+            }
+        } else {
+            null
+        }
+
+        editText.setCompoundDrawables(null, null, errorIcon, null)
+        editText.setBackgroundResource(if (isError) R.drawable.error_edit_text_background else R.drawable.email_login)
+
+        errorTextView.text = errorMessage
+        errorTextView.visibility = if (isError) View.VISIBLE else View.GONE
+    }
+
 
     private fun authenticator(email: String, senha: String) {
         auth.signInWithEmailAndPassword(email, senha)
@@ -137,6 +199,11 @@ class loginScreen : AppCompatActivity() {
                     } else {
                         "Autenticação falhou, verifique se o Email e senha estã corretos. " +
                                 " Ou tente de novo mais tarde."                    }
+                    if (exception is FirebaseAuthInvalidCredentialsException) {
+                        val errorMessage2 = "Senha inválida."
+                        updateInputState(etSenhaLogin, tvESenhaLoginError, errorMessage2, true)
+                    }
+
                     Log.w(ContentValues.TAG, "signInWithEmail:failure", exception)
                     Toast.makeText(baseContext, errorMessage, Toast.LENGTH_SHORT).show()
                 }
@@ -237,4 +304,15 @@ class loginScreen : AppCompatActivity() {
                 }
         }
     }
+
+    private fun clearErrorOnTextChange(editText: AppCompatEditText, errorTextView: AppCompatTextView) {
+        editText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable?) {
+                updateInputState(editText, errorTextView, "", false)
+            }
+        })
+    }
+
 }
